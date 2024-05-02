8779dab1fe9ffd7f00a9e9cded39598e917c5891
sakinurs <sakinurs@mail.ru>
2024-05-03 01:17:44 +0600

    fix import logo for deploy

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,15 +1,13 @@
+import carLogo from '@assets/car-logo.svg'; // Import the image using the correct alias
 import { AppRoutes } from '@enums/routes';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <Link to={AppRoutes.HOME}>
-        <img
-          className={styles.logo}
-          src="/test-cars/src/assets/car-logo.svg"
-          alt="car-logo"
-        />
+        <img className={styles.logo} src={carLogo} alt="car-logo" />
       </Link>
     </header>
   );
