8fdbff1ed7d1fbe7609ff9a63ed1a575e157784c
sakinurs <sakinurs@mail.ru>
2024-05-03 01:03:39 +0600

    fix routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,16 +3,19 @@ import { MainLayout } from '@features/layout';
 import { TransportsPage } from '@pages/transports';
 import { createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
-  {
-    path: AppRoutes.HOME,
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        path: AppRoutes.TRANSPORTS_BY_CATEGORY,
-        element: <TransportsPage />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: AppRoutes.HOME,
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          path: AppRoutes.TRANSPORTS_BY_CATEGORY,
+          element: <TransportsPage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/test-cars' },
+);
